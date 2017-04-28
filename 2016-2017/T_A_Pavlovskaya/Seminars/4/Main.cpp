1ae0558e54dff611faad0847caa5a8f750fbd0ac
BeYkeRYkt <beykerykt@gmail.com>
2017-04-28 23:26:31 +0700

    Improvement T_A_Pavlovskaya\4\13

--- 2016-2017/T_A_Pavlovskaya/Seminars/4/Main.cpp
+++ 2016-2017/T_A_Pavlovskaya/Seminars/4/Main.cpp
@@ -2,11 +2,22 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
 int noteSize = 8;
+int availableSize = 0;
 Note* notes = new Note[noteSize];
+
+/* Sorting */
+void sort(Note* note, int N){
+	for (int i = 0; i < N - 1; i++)
+	for (int j = i + 1; j < N; j++){
+		if (strcmp(note[i].getFamilyName().c_str(), note[j].getFamilyName().c_str())>0) swap(note[i], note[j]);
+	}
+}
+
 
 void inputNote(int index){
 	cin.clear(); //fix
@@ -34,6 +45,9 @@ void inputNote(int index){
 	cin >> phone;
 	notes[index].setPhoneNumber(phone);
 	cout << endl;
+
+	availableSize++;
+	sort(notes, availableSize);
 }
 
 void inputInformation()
@@ -54,7 +68,19 @@ void search(int month){
 	}
 }
 
+void viewAllNotes(){
+	cout << "All notes: " << endl;
+	for (int i = 0; i < availableSize; i++){
+		cout << notes[i].getFamilyName() << " " << notes[i].getName() << " " << notes[i].getPhoneNumber() << " ";
+		cout << notes[i].getDate()[0] << ".";
+		cout << notes[i].getDate()[1] << ".";
+		cout << notes[i].getDate()[2] << endl;
+	}
+}
+
 int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	bool run = true;
 	while (run){
 		cin.clear(); //fix
@@ -62,6 +88,7 @@ int main(){
 		cout << "1. Create 8 notes" << endl;
 		cout << "2. Create note in index" << endl;
 		cout << "3. Search note from month" << endl;
+		cout << "4. View all notes" << endl;
 		cout << endl << "--> ";
 
 		int code = 0;
@@ -88,6 +115,9 @@ int main(){
 				   search(month);
 				   break;
 		}
+		case 4:
+			viewAllNotes();
+			break;
 		default:
 			cout << "Unknown command" << endl;
 			break;
