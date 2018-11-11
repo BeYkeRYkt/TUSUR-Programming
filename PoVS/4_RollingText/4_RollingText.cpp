05c5955c68f77f1d93bfdfb7aef1b4586a3a80f0
BeYkeRYkt <beykerykt@gmail.com>
2018-11-11 21:21:52 +0700

    TUSUR: PoVS: 4_RollingText: Update
    * Center the window during startup
    * Simplify text rotation
    * Set time to 500 mc
    * use LPARAM for GWL_USERDATA
    * Process WM_LBUTTONDOWN and WM_RBUTTONDOWN messages
    
    Change-Id: I1e91192a94facd0f8ac74ae76942c79444ec7b77
    Signed-off-by: BeYkeRYkt <beykerykt@gmail.com>

--- PoVS/4_RollingText/4_RollingText.cpp
+++ PoVS/4_RollingText/4_RollingText.cpp
@@ -23,7 +23,6 @@
 */
 #include "stdafx.h"
 #include "4_RollingText.h"
-#include "iostream"
 
 // Names
 #define ClazzName "4_RollingText"
@@ -51,9 +50,14 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance,
 	if (!RegisterClass(&wcex))
 		return 0;
 
-	HMENU hMenu = LoadMenu(NULL, MAKEINTRESOURCE(IDC_MY4_ROLLINGTEXT)); // загрузing менu from ресурсов
+	int CX = (GetSystemMetrics(SM_CXSCREEN));
+	int CY = (GetSystemMetrics(SM_CYSCREEN));
+	int X = CX / 5;
+	int Y = CY / 5;
+
+	HMENU hMenu = LoadMenu(NULL, MAKEINTRESOURCE(IDC_MY4_ROLLINGTEXT));
 	HWND  mainWindow = CreateWindow(wcex.lpszClassName, MW, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, hMenu, hInstance, NULL);
+		X, Y, CX - X * 2, CY - 2 * Y, NULL, hMenu, hInstance, NULL);
 
 	if (mainWindow == NULL)
 		return 0;
@@ -68,88 +72,90 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance,
 	return 0;
 }
 
-/*
-* Source: https://www.codeproject.com/Articles/740/Simple-Text-Rotation
-*/
-void DrawRotatedText(HDC hdc, char *str, LPRECT rect,
-	double angle, UINT nOptions = 0)
-{
-	// convert angle to radian
-	double pi = 3.141592654;
-	double radian = pi * 2 / 360 * angle;
-
-	// get the center of a not-rotated text
-	SIZE TextSize;
-	GetTextExtentPoint32(hdc, str, strlen(str), &TextSize); //вычисляет ширину и высоту заданной строки текста.
-
-	POINT center;
-	center.x = TextSize.cx / 2;
-	center.y = TextSize.cy / 2;
-
-	// now calculate the center of the rotated text
-	POINT rcenter;
-	rcenter.x = long(cos(radian) * center.x - sin(radian) * center.y);
-	rcenter.y = long(sin(radian) * center.x + cos(radian) * center.y);
-
-	// finally draw the text and move it to the center of the rectangle
-	SetTextAlign(hdc, TA_BASELINE);
-	SetBkMode(hdc, TRANSPARENT);
-	ExtTextOut(hdc, rect->left + (rect->right - rect->left) / 2 - rcenter.x,
-		rect->top + (rect->bottom - rect->top) / 2 + rcenter.y,
-		nOptions, rect, str, strlen(str), NULL);
-}
-
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message){
 	case WM_CREATE:
-		EnableMenuItem(GetMenu(hWnd), IDM_START, MF_DISABLED);
-		SetTimer(hWnd, 0, 1000, NULL); // Запуск таймера на 1 секунду (1000 мс)
+		if (SetTimer(hWnd, 0, 500, NULL)){
+			EnableMenuItem(GetMenu(hWnd), IDM_START, MF_DISABLED);
+		}
+		SetWindowLong(hWnd, GWL_USERDATA, MAKELPARAM(0, 20));
 		break;
-	case WM_TIMER: { // Событие которое вызывается таймером
-					   InvalidateRect(hWnd, NULL, true);
-					   double old_angle = (double)GetWindowLong(hWnd, GWL_USERDATA);
-					   double next_angle = 10; // 10 градусов
+	case WM_TIMER: {
+					   LPARAM lparam = (LPARAM) GetWindowLong(hWnd, GWL_USERDATA);
+					   double old_angle = (double) LOWORD(lparam);
+					   double next_angle = 10;
 					   double new_angle = old_angle + next_angle;
 					   if (new_angle >= 360){
-						   new_angle = 0;
+						   new_angle -= 360;
 					   }
-					   SetWindowLong(hWnd, GWL_USERDATA, (LONG) new_angle);
+					   lparam = MAKELPARAM(new_angle, HIWORD(lparam));
+					   SetWindowLong(hWnd, GWL_USERDATA, (LONG) lparam);
+					   InvalidateRect(hWnd, NULL, true);
 	}
 		break;
 	case WM_SIZE:
 		InvalidateRect(hWnd, NULL, true);
+		return 0;
+	case WM_LBUTTONDOWN: {
+							 LPARAM lparam = (LPARAM) GetWindowLong(hWnd, GWL_USERDATA);
+							 int font_size = (int) HIWORD(lparam);
+							 font_size += 10;
+							 lparam = MAKELPARAM(LOWORD(lparam), font_size);
+							 SetWindowLong(hWnd, GWL_USERDATA, (LONG) lparam);
+							 InvalidateRect(hWnd, NULL, true);
+	}
+		break;
+	case WM_RBUTTONDOWN: {
+							 LPARAM lparam = (LPARAM) GetWindowLong(hWnd, GWL_USERDATA);
+							 int font_size = (int) HIWORD(lparam);
+							 font_size -= 10;
+							 if (font_size <= 0){
+								 font_size = 1;
+							 }
+							 lparam = MAKELPARAM(LOWORD(lparam), font_size);
+							 SetWindowLong(hWnd, GWL_USERDATA, (LONG) lparam);
+							 InvalidateRect(hWnd, NULL, true);
+	}
 		break;
 	case WM_PAINT: {
+					   // Default
 					   PAINTSTRUCT ps;
 					   HDC hdc = ::BeginPaint(hWnd, &ps);
 					   RECT rc;
 					   GetClientRect(hWnd, &rc);
-					   HANDLE hfnt, hfntPrev;
 
 					   // create font
+					   HANDLE hfnt, hfntPrev;
 					   LOGFONT lf;
-					   ZeroMemory(&lf, sizeof(LOGFONT)); //clean memory for logfont
-					   double angle = (double)GetWindowLong(hWnd, GWL_USERDATA);
-					   lf.lfHeight = 30;
+					   ZeroMemory(&lf, sizeof(LOGFONT)); //clean memory for logicfont
+					   LPARAM lparam = (LPARAM) GetWindowLong(hWnd, GWL_USERDATA);
+					   double angle = (double) LOWORD(lparam);
+					   int font_size = (int) HIWORD(lparam);
+					   lf.lfHeight = font_size;
 					   lf.lfWeight = FW_NORMAL;
-					   lf.lfEscapement = angle * 10; // Для винды необходимо увеличить угол в 10 раз
-					   lf.lfOrientation = angle * 10;
+					   lf.lfEscapement = (long) angle * 10;
+					   lf.lfOrientation = (long) angle * 10;
+					   strcpy_s(lf.lfFaceName, "Times New Roman");
 					   hfnt = CreateFontIndirect(&lf);
 					   hfntPrev = SelectObject(hdc, hfnt);
 
-					   //start drawning
+					   // start drawning
 					   TCHAR* str = TEXT("[SAMPLE TEXT]");
-					   DrawRotatedText(hdc, str, &rc, angle);
+					   SetBkMode(hdc, TRANSPARENT);
+					   SetTextAlign(hdc, TA_CENTER | TA_BASELINE);
+					   int x = (rc.right - rc.left) / 2;
+					   int y = (rc.bottom - rc.top) / 2;
+					   TextOut(hdc, x, y, str, strlen(str));
 
+					   // Drop all
+					   SetBkMode(hdc, OPAQUE);
 					   SelectObject(hdc, hfntPrev);
 					   DeleteObject(hfnt);
-
-					   SetBkMode(hdc, OPAQUE);
 					   ::EndPaint(hWnd, &ps);
 	}
 		break;
-	case WM_COMMAND: // Команды из меню
+	case WM_COMMAND:
 	{
 					   switch (wParam)
 					   {
@@ -158,13 +164,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 						   PostQuitMessage(0);
 						   break;
 					   case IDM_STOP:
-						   if (KillTimer(hWnd, 0)){ // Хз, нужна ли это проверка или нет
+						   if (KillTimer(hWnd, 0)){
 							   EnableMenuItem(GetMenu(hWnd), IDM_STOP, MF_DISABLED);
 							   EnableMenuItem(GetMenu(hWnd), IDM_START, MF_ENABLED);
 						   }
 						   break;
 					   case IDM_START:
-						   if (SetTimer(hWnd, 0, 1000, NULL)){ // Запуск таймера на 1 секунду (1000 мс)
+						   if (SetTimer(hWnd, 0, 500, NULL)){
 							   EnableMenuItem(GetMenu(hWnd), IDM_START, MF_DISABLED);
 							   EnableMenuItem(GetMenu(hWnd), IDM_STOP, MF_ENABLED);
 						   }
